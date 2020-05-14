bba993886a7785fdd5bb7d205e935c515fecac4f
Haiz3n <lfrguimaraes3@gmail.com>
2020-05-14 08:16:52 -0300

    Minha calculadora

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-x = 1
+#adicao
+print(3 + 2)
+
+#subtracao
+print(4 - 1)
+
+#multiplicacao
+print(8 * 5)
+
+#divisao
+print(8 / 2)
+
+#potenciacao
+print(4 ** 3)
+
+#radiciacao
+raiz = 9 ** (1/2)  
+print(raiz)
+
+#mude os numeros como quizer(somente nao mude o (1/2) da radiciacao)
