c7bc68b520423a2a3d308a5424ad0462abdbe458
Haiz3n <lfrguimaraes3@gmail.com>
2020-05-16 11:38:07 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+def soma(x, y):
+    return x + y
+Calc1 = soma(2, 3)
+
+def subtracao(x, y):
+    return x - y
+Calc2 = subtracao(5, 2)
+
+def divisao(x, y):
+    return x / y
+Calc3 = divisao(4, 2)
+
+def multiplicacao(x, y):
+    return x * y
+Calc4 = multiplicacao(5, 2)
+
+def potenciacao(x, y):
+    return x ** y
+Calc5 = potenciacao(5, 2)
+
+import math
+Calc6 = math.sqrt(36)
+
+print(Calc1)
+print(Calc2)
+print(Calc3)
+print(Calc4)
+print(Calc5)
+print(Calc6)
